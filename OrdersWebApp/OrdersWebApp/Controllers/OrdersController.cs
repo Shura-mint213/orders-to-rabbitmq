bc4eb3048636596f23bc0f313405dc167d7c8965
unknown <fomov0202@bk.ru>
2024-05-29 12:39:21 +0300

    start

---
+++ OrdersWebApp/OrdersWebApp/Controllers/OrdersController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Shared.Interfaces;
+
+namespace OrdersWebApp.Controllers
+{
+    [ApiController]
+    [Route("Orders")]
+    public class OrdersController : Controller
+    {
+        private readonly IRabbitMqService _rabbitMqService;
+        public OrdersController(IRabbitMqService rabbitMqService) 
+        {
+            _rabbitMqService = rabbitMqService;
+        }
+
+        [HttpPost]
+        [Route("Message/String")]
+        public IActionResult MessageString(string msg)
+        {
+            _rabbitMqService.SendMessage(msg);
+            return Ok();
+        }
+    }
+}
---
+++ OrdersWebApp/OrdersWebApp/Extensions/ServiceProviderExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Producer;
+using Shared.Interfaces;
+
+namespace OrdersWebApp.Extensions
+{
+    public static class ServiceProviderExtensions
+    {
+        /// <summary>
+        /// Регистрирует сервисы для работы с RabbitMQ.
+        /// </summary>
+        /// <param name="services">Коллекция, в которой хранятся все зарегистрированные сервисы</param>
+        public static void AddProducerServices(this IServiceCollection services)
+        {
+            services.AddTransient<IRabbitMqService, RabbitMqService>();
+        }
+    }
+}
